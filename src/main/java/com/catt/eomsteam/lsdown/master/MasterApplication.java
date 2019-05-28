2c8abf87a0b1ee388edd4acaf07e83bf0c25b1d4
hua <m15622201762@163.com>
2019-05-28 20:47:46 +0800

    Initial commit

---
+++ src/main/java/com/catt/eomsteam/lsdown/master/MasterApplication.java
@@ -0,0 +1,13 @@
+package com.catt.eomsteam.lsdown.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterApplication.class, args);
+    }
+
+}
