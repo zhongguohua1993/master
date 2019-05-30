c9640b5dc8eb48e800b79eddce551dea513d0781
hua <m15622201762@163.com>
2019-05-30 22:20:47 +0800

    日志整合

--- src/main/java/com/catt/eomsteam/lsdown/master/MasterApplication.java
+++ src/main/java/com/catt/eomsteam/lsdown/master/MasterApplication.java
@@ -4,6 +4,8 @@ import com.catt.common.utils.SpringUtil;
 import com.catt.eomsteam.lsdown.base.running.RunningService;
 import com.catt.eomsteam.lsdown.master.app.lsst.LsstRunningService;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,8 +19,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MasterApplication {
 
   public static void main(String[] args) {
+    Logger logger = LoggerFactory.getLogger(MasterApplication.class);
     SpringApplication.run(MasterApplication.class, args);
+    logger.info("before");
     RunningService lsstRunningService = SpringUtil.getBean(LsstRunningService.class);
     lsstRunningService.start();
+    logger.info("end");
   }
 }
